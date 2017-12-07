8f989f056f7ad21412c62f90bb01937a87992f68
Juanjo Sierra <jjsierra103@gmail.com>
2017-12-07 17:07:08 +0100

    Another case study analyzing summer accidents

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ crash_accidents_sample = accidents[accidents['TIPO_ACCIDENTE'].str.contains("Col
 # Select accidents between 6AM and 12PM
 morning_accidents_sample = accidents.loc[(accidents['HORA']>=6) & (accidents['HORA']<=12)]
 
-# Select non-deadly accidents
-#accidents_sample = accidents.loc[accidents['TOT_MUERTOS']==0]
+# Select accidents in summertime (June, July and August)
+summer_accidents_sample = accidents.loc[(accidents['MES']>=6) & (accidents['MES']<=8)]
 
 # Select interesting variables for clustering
 selected = ['TOT_VICTIMAS', 'TOT_MUERTOS', 'TOT_HERIDOS_GRAVES', 'TOT_HERIDOS_LEVES', 'TOT_VEHICULOS_IMPLICADOS']
@@ -25,7 +25,9 @@ selected = ['TOT_VICTIMAS', 'TOT_MUERTOS', 'TOT_HERIDOS_GRAVES', 'TOT_HERIDOS_LE
 # Subset with 'selected' variables
 crash_accidents_subset = crash_accidents_sample[selected]
 morning_accidents_subset = morning_accidents_sample[selected]
+summer_accidents_subset = summer_accidents_sample[selected]
 
 # Clustering Algorithms for each of the selected subsets
 #clustering.ClusteringAlgorithms(crash_accidents_subset, 5000, "Crash accidents")
-clustering.ClusteringAlgorithms(morning_accidents_subset, 5000, "Morning accidents")
+#clustering.ClusteringAlgorithms(morning_accidents_subset, 5000, "Morning accidents")
+clustering.ClusteringAlgorithms(summer_accidents_subset, 5000, "Summer accidents")
