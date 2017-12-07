5fd8e034308cb4c76a80e15eb489d10bda36688a
Juanjo Sierra <jjsierra103@gmail.com>
2017-12-07 13:21:19 +0100

    Separated datasets for distinguishing themselves from each other when it comes to call the algorithms function

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import clustering
 accidents = pd.read_csv('accidentes_2013.csv')
 
 # Select "crash" type accidents
-accidents_sample = accidents[accidents['TIPO_ACCIDENTE'].str.contains("Colisión de vehículos")]
+crash_accidents_sample = accidents[accidents['TIPO_ACCIDENTE'].str.contains("Colisión de vehículos")]
 
 # Select accidents between 6AM and 12PM
 #accidents_sample = accidents.loc[(accidents['HORA']>=6) & (accidents['HORA']<=12)]
@@ -21,29 +21,9 @@ accidents_sample = accidents[accidents['TIPO_ACCIDENTE'].str.contains("Colisión
 
 # Select interesting variables for clustering
 #selected = ['HORA', 'DIASEMANA', 'TOT_VICTIMAS', 'TOT_MUERTOS', 'TOT_HERIDOS_GRAVES', 'TOT_HERIDOS_LEVES', 'TOT_VEHICULOS_IMPLICADOS']
-selected = ['TOT_VICTIMAS', 'TOT_MUERTOS', 'TOT_HERIDOS_GRAVES', 'TOT_HERIDOS_LEVES', 'TOT_VEHICULOS_IMPLICADOS']
+crash_selected = ['TOT_VICTIMAS', 'TOT_MUERTOS', 'TOT_HERIDOS_GRAVES', 'TOT_HERIDOS_LEVES', 'TOT_VEHICULOS_IMPLICADOS']
 
 # Subset with 'selected' variables
-accidents_subset = accidents_sample[selected]
+crash_accidents_subset = crash_accidents_sample[selected]
 
-clustering.ClusteringAlgorithms(accidents_subset, 5000, "Crash accidents")
-
-'''
-# assignment gets turned into DataFrame
-kmeans_clusters = pd.DataFrame(kmeans_predict,index=accidents_subset.index,columns=['kmeans_clusters'])
-'''
-
-'''
-#y se añade como columna a X
-X_kmeans = pd.concat([accidents_subset, clusters], axis=1)
-
-print("---------- Preparando el scatter matrix...")
-import seaborn as sns
-sns.set()
-variables = list(X_kmeans)
-variables.remove('cluster')
-sns_plot = sns.pairplot(X_kmeans, vars=variables, hue="cluster", palette='Paired', plot_kws={"s": 25}, diag_kind="hist") #en hue indicamos que la columna 'cluster' define los colores
-sns_plot.fig.subplots_adjust(wspace=.03, hspace=.03);
-sns_plot.savefig("kmeans.png")
-print("")
-'''
+clustering.ClusteringAlgorithms(crash_accidents_subset, 5000, "Crash accidents")
