76589b43ae1beee97d85e300e8e09c8d6d3bbbde
Juanjo Sierra <jjsierra103@gmail.com>
2017-12-07 18:41:21 +0100

    Deleted Mean-shift and MiniBatch K-Means algorithms from the study

--- clustering.py
+++ clustering.py
@@ -9,7 +9,7 @@ import seaborn as sns
 import pandas as pd
 import os
 
-from sklearn.cluster import KMeans, MiniBatchKMeans, MeanShift, DBSCAN, Birch, SpectralClustering, AgglomerativeClustering, estimate_bandwidth
+from sklearn.cluster import KMeans, DBSCAN, Birch, SpectralClustering, AgglomerativeClustering
 from sklearn import metrics
 from sklearn import preprocessing
 
@@ -28,14 +28,6 @@ def ClusteringAlgorithms(dataset, samples, dataset_name):
     # K-Means
     k_means = KMeans(init='k-means++', n_clusters=4, n_init=5)
 
-    # MiniBatch K-Means
-    mbkm = MiniBatchKMeans(init='k-means++', n_clusters=4, n_init=5)
-
-    # Estimate bandwidth for Mean Shift Algorithm
-    bandwidth = estimate_bandwidth(normalized_dataset, quantile=0.2, n_samples=500)
-    # Mean Shift
-    mean_shift = MeanShift(bandwidth=bandwidth, bin_seeding=True)
-
     # DBSCAN
     dbscan = DBSCAN(eps=0.1)
 
@@ -50,8 +42,6 @@ def ClusteringAlgorithms(dataset, samples, dataset_name):
 
     clustering_algorithms = [
         ("K-Means", k_means),
-        ("MiniBatch K-Means", mbkm),
-        ("Mean Shift", mean_shift),
         ("DBSCAN", dbscan),
         ("Birch", birch),
         ("Spectral Clustering", spectral),
@@ -67,7 +57,7 @@ def ClusteringAlgorithms(dataset, samples, dataset_name):
         t2 = time.time() - t1
         k = len(set(cluster_predict))
         print("| k: {:3.0f}, ".format(k),end='')
-        print("{:0.2f} segundos, ".format(t2),end='')
+        print("{:0.2f} seconds, ".format(t2),end='')
         if (k>1) and (name is not "Ward"):
             metric_CH = metrics.calinski_harabaz_score(normalized_dataset, cluster_predict)
             metric_SC = metrics.silhouette_score(normalized_dataset, cluster_predict, metric='euclidean', sample_size=floor(0.1*len(normalized_dataset)), random_state=123456)
