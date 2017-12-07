740f9c95c00ff6a83a8a1ad0fe13fd0dbd17b02e
Juanjo Sierra <jjsierra103@gmail.com>
2017-12-07 18:32:54 +0100

    Changed crash accidents case study for highway accidents

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import clustering
 
 accidents = pd.read_csv('accidentes_2013.csv')
 
-# Select "crash" type accidents
-crash_accidents_sample = accidents[accidents['TIPO_ACCIDENTE'].str.contains("Colisión de vehículos")]
+# Select accidents in highways and freeways
+highway_accidents_sample = accidents[accidents['TIPO_VIA'].str.contains("AUTO")]
 
 # Select accidents between 6AM and 12PM
 morning_accidents_sample = accidents.loc[(accidents['HORA']>=6) & (accidents['HORA']<=12)]
@@ -23,11 +23,11 @@ summer_accidents_sample = accidents.loc[(accidents['MES']>=6) & (accidents['MES'
 selected = ['TOT_VICTIMAS', 'TOT_MUERTOS', 'TOT_HERIDOS_GRAVES', 'TOT_HERIDOS_LEVES', 'TOT_VEHICULOS_IMPLICADOS']
 
 # Subset with 'selected' variables
-crash_accidents_subset = crash_accidents_sample[selected]
+highway_accidents_subset = highway_accidents_sample[selected]
 morning_accidents_subset = morning_accidents_sample[selected]
 summer_accidents_subset = summer_accidents_sample[selected]
 
 # Clustering Algorithms for each of the selected subsets
-#clustering.ClusteringAlgorithms(crash_accidents_subset, 5000, "Crash accidents")
+clustering.ClusteringAlgorithms(highway_accidents_subset, 5000, "Highway accidents")
 #clustering.ClusteringAlgorithms(morning_accidents_subset, 5000, "Morning accidents")
-clustering.ClusteringAlgorithms(summer_accidents_subset, 5000, "Summer accidents")
+#clustering.ClusteringAlgorithms(summer_accidents_subset, 5000, "Summer accidents")
