6da40b41b56ac383a33f0708586c6bb3508a2f85
Juanjo Sierra <jjsierra103@gmail.com>
2017-12-07 13:59:12 +0100

    New dataset: accidents that happened in the morning

--- main.py
+++ main.py
@@ -14,16 +14,18 @@ accidents = pd.read_csv('accidentes_2013.csv')
 crash_accidents_sample = accidents[accidents['TIPO_ACCIDENTE'].str.contains("Colisión de vehículos")]
 
 # Select accidents between 6AM and 12PM
-#accidents_sample = accidents.loc[(accidents['HORA']>=6) & (accidents['HORA']<=12)]
+morning_accidents_sample = accidents.loc[(accidents['HORA']>=6) & (accidents['HORA']<=12)]
 
 # Select non-deadly accidents
 #accidents_sample = accidents.loc[accidents['TOT_MUERTOS']==0]
 
 # Select interesting variables for clustering
-#selected = ['HORA', 'DIASEMANA', 'TOT_VICTIMAS', 'TOT_MUERTOS', 'TOT_HERIDOS_GRAVES', 'TOT_HERIDOS_LEVES', 'TOT_VEHICULOS_IMPLICADOS']
-crash_selected = ['TOT_VICTIMAS', 'TOT_MUERTOS', 'TOT_HERIDOS_GRAVES', 'TOT_HERIDOS_LEVES', 'TOT_VEHICULOS_IMPLICADOS']
+selected = ['TOT_VICTIMAS', 'TOT_MUERTOS', 'TOT_HERIDOS_GRAVES', 'TOT_HERIDOS_LEVES', 'TOT_VEHICULOS_IMPLICADOS']
 
 # Subset with 'selected' variables
-crash_accidents_subset = crash_accidents_sample[crash_selected]
+crash_accidents_subset = crash_accidents_sample[selected]
+morning_accidents_subset = morning_accidents_sample[selected]
 
-clustering.ClusteringAlgorithms(crash_accidents_subset, 5000, "Crash accidents")
+# Clustering Algorithms for each of the selected subsets
+#clustering.ClusteringAlgorithms(crash_accidents_subset, 5000, "Crash accidents")
+clustering.ClusteringAlgorithms(morning_accidents_subset, 5000, "Morning accidents")
