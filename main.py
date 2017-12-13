a1dbecb744c945088d71443f8fdec97ae65719b6
Juanjo Sierra <jjsierra103@gmail.com>
2017-12-13 11:53:01 +0100

    Two case study changed, one of them has really reduced its size for making dendrograms out of it

--- main.py
+++ main.py
@@ -13,21 +13,27 @@ accidents = pd.read_csv('accidentes_2013.csv')
 # Select accidents in highways and freeways
 highway_accidents_sample = accidents[accidents['TIPO_VIA'].str.contains("AUTO")]
 
-# Select accidents between 6AM and 12PM
-morning_accidents_sample = accidents.loc[(accidents['HORA']>=6) & (accidents['HORA']<=12)]
+# Select accidents between between 0:00 AM and 6:00 AM
+early_morning_accidents_sample = accidents.loc[(accidents['HORA']>=0) & (accidents['HORA']<=6)]
 
-# Select accidents in summertime (June, July and August)
-summer_accidents_sample = accidents.loc[(accidents['MES']>=6) & (accidents['MES']<=8)]
+# Select accidents that happened in Andalucía, when the road was wet and the vehicle overturned
+wet_overturned_accidents_sample = accidents.loc[(accidents.COMUNIDAD_AUTONOMA.str.contains("Andalucía")) &
+                       (accidents.SUPERFICIE_CALZADA.str.contains("MOJADA")) &
+                       (accidents.TIPO_ACCIDENTE.str.contains("Vuelco"))]
 
 # Select interesting variables for clustering
 selected = ['TOT_VICTIMAS', 'TOT_MUERTOS', 'TOT_HERIDOS_GRAVES', 'TOT_HERIDOS_LEVES', 'TOT_VEHICULOS_IMPLICADOS']
 
 # Subset with 'selected' variables
 highway_accidents_subset = highway_accidents_sample[selected]
-morning_accidents_subset = morning_accidents_sample[selected]
-summer_accidents_subset = summer_accidents_sample[selected]
+early_morning_accidents_subset = early_morning_accidents_sample[selected]
+wet_overturned_accidents_subset = wet_overturned_accidents_sample[selected]
+
+print(highway_accidents_subset.shape)
+print(early_morning_accidents_subset.shape)
+print(wet_overturned_accidents_subset.shape)
 
 # Clustering Algorithms for each of the selected subsets
-clustering.ClusteringAlgorithms(highway_accidents_subset, 5000, "Highway accidents")
-#clustering.ClusteringAlgorithms(morning_accidents_subset, 5000, "Morning accidents")
-#clustering.ClusteringAlgorithms(summer_accidents_subset, 5000, "Summer accidents")
+#clustering.ClusteringAlgorithms(highway_accidents_subset, 5000, "Highway accidents")
+clustering.ClusteringAlgorithms(early_morning_accidents_subset, 5000, "Morning accidents")
+#clustering.ClusteringAlgorithms(wet_overturned_accidents_subset, 5000, "Summer accidents")
